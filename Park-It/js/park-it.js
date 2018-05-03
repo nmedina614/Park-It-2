6fd10e3da527b54fa9fb0bf307b9040ae0237881
Tony <tony.g.thompson@gmail.com>
2018-05-03 11:42:59 -0700

    moved initMap

--- Park-It/js/park-it.js
+++ Park-It/js/park-it.js
@@ -17,3 +17,20 @@ $('#gotIt').click(function(){
     $('#instructions').hide();
 
 });
+
+function initMap() {
+    var grc = {lat: 47.313582, lng: -22.1800072} ;
+    var tokyo = {lat: 35.6895, lng:  139.6917} ;
+    var mapDiv = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: tokyo
+    });
+    var marker = new google.maps.Marker({
+        position: tokyo,
+        map: mapDiv
+
+    });
+
+
+
+}
